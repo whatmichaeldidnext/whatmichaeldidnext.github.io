081e6dbd595f21dad6447e2b61653a8f7326447f
mcharles <michael.charles.again@gmail.com>
2026-06-25 15:40:58 +0100

    Github Pages work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	site: 'https://what-michael-did-next.co.uk',
+	integrations: [
+		sitemap()
+	]
 });
